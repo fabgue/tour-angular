0a31e6a21ad6995029d5b34c81c8df9b74713c63
Fabio Guerrero <fabgue@ymail.com>
2017-11-06 18:20:24 -0500

    Tutorial part6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
@@ -7,13 +7,14 @@ import { HeroesModule } from './heroes/heroes.module';
 
 import { HeroService } from './services/hero.service';
 import { MessageService } from './services/message.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HeroesModule
+    BrowserModule, HeroesModule, AppRoutingModule
   ],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; 
+import { RouterModule } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule
+    CommonModule, FormsModule, RouterModule
   ],
-  declarations: [HeroesComponent, HeroDetailComponent, MessagesComponent],
+  declarations: [HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
   exports: [HeroesComponent, MessagesComponent]
 })
 export class HeroesModule { }
