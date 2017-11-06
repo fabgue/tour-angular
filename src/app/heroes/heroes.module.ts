dc08e9a2a719d4dc0f5dbfab33d518e596904824
Fabio Guerrero <fabgue@ymail.com>
2017-11-06 17:25:58 -0500

    Tutorial part4

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; 
 
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   imports: [
     CommonModule, FormsModule
   ],
-  declarations: [HeroesComponent],
+  declarations: [HeroesComponent, HeroDetailComponent],
   exports: [HeroesComponent]
 })
 export class HeroesModule { }
