58a677614e37579af84508a0a8ab7cba34f27270
Fabio Guerrero <fabgue@ymail.com>
2017-11-06 19:27:37 -0500

    Tutorial part7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 import { HeroesModule } from './heroes/heroes.module';
-
 import { HeroService } from './services/hero.service';
 import { MessageService } from './services/message.service';
-import { AppRoutingModule } from './/app-routing.module';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './heroes/in-memory-data.service';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HeroesModule, AppRoutingModule
+    BrowserModule,     
+    AppRoutingModule,
+    HttpClientModule,
+    HeroesModule, 
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.    
+    HttpClientInMemoryWebApiModule.forRoot( InMemoryDataService, { dataEncapsulation: false } )
   ],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,12 +7,13 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   imports: [
     CommonModule, FormsModule, RouterModule
   ],
-  declarations: [HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
+  declarations: [HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent, HeroSearchComponent],
   exports: [HeroesComponent, MessagesComponent]
 })
 export class HeroesModule { }
